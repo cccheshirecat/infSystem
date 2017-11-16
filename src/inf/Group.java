cfe99aa7c634eeabefe73e373d178633eec8df45
Cheshire <ivanova.tanya97@mail.ru>
2017-11-16 19:41:47 +0400

    Student and Group

---
+++ src/inf/Group.java
@@ -0,0 +1,61 @@
+package inf;
+
+import java.util.ArrayList;
+
+public class Group {
+    private String name;
+    private ArrayList<Student> students;
+    private Student leader;
+    public Group(String name, ArrayList<Student> students, Student leader){
+        this.name=name;
+        this.students=students;
+        this.leader=leader;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+    public void deleteStudent(String name, String sname){
+        String curntName;
+        String curntSname;
+        int count=0;
+        for (Student student :
+                students) {
+            curntName=student.getName();
+            curntSname=student.getSname();
+            if (curntSname.equals(sname)){
+                if (curntName.equals(name)){
+                    students.remove(count);
+                    break;
+                }
+            }
+            count++;
+        }
+    }
+    public void addStudent(Student student){
+        students.add(student);
+    }
+    public int studentsNum(){
+        return students.size();
+    }
+}
--- src/inf/Student.java
+++ src/inf/Student.java
@@ -1,4 +1,60 @@
 package inf;
 
+import java.util.HashMap;
+
 public class Student {
+    private String name;
+    private String sname;
+    private int age;
+    public int course;
+    private HashMap<String,String> rating=new HashMap();
+    public Student(String name, String sname, int age, int course){
+        this.age=age;
+        this.name=name;
+        this.sname=sname;
+    }
+    public void setSubjectRating(String subject, String rating){
+        this.rating.put(subject,rating);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+    public String getRating(String subject){
+        return  rating.get(subject);
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setSname(String sname){
+        this.sname=name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setRating(HashMap<String, String> rating) {
+        this.rating = rating;
+    }
+
+    public HashMap<String, String> getRating() {
+        return rating;
+    }
+
+    public int getCourse() {
+        return course;
+    }
 }
